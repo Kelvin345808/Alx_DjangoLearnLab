81502bfa5b18736e3630cc9152a048aef1278950
kelvin nzau <kelvinnzau10@gmail.com>
2025-02-16 14:00:49 +0300

    models.py

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -12,5 +12,4 @@ class Book(models.Model):
         return f"{self.title} by {self.author} ({self.publication_year})"
 from bookshelf.models import Book
 
-# Create a new book instance
-book = Book.objects.create(title="1984", author="George Orwell", publication_year=1949)
+
