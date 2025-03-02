728c8d08fe38a2d72bc4f9f5359787627be0563d
kelvin nzau <kelvinnzau10@gmail.com>
2025-03-02 18:45:47 +0300

    views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404
 from .models import Article
+from .models import Book
 
 # Create your views here.
 
@@ -25,3 +26,8 @@ def delete_article(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     article.delete()
     return redirect("article_list")
+ 
+ @permission_required("bookshelf.can_view", raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()  # Fetch all books from the database
+    return render(request, "bookshelf/book_list.html", {"books": books})
