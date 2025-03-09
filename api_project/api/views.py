650031654346efd8ae722f89aeb064deefe66157
kelvin nzau <kelvinnzau10@gmail.com>
2025-03-09 17:10:25 +0300

    views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,6 @@ from .models import Book
 from .serializers import BookSerializer
 
 # Define the BookList view
-class BookList(ListAPIView):
+class BookList("generics.ListAPIView"):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
